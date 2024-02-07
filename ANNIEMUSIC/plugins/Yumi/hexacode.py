29b1b17dfb2c5a8afcc340f3692138498a42cef7
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-07 22:11:52 +0530

    Add files via upload

---
+++ ANNIEMUSIC/plugins/Yumi/hexacode.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+from ANNIEMUSIC import app
+from config import BOT_USERNAME
+
+
+def hex_to_text(hex_string):
+    try:
+        text = bytes.fromhex(hex_string).decode('utf-8')
+        return text
+    except Exception as e:
+        return f"Error decoding hex: {str(e)}"
+
+
+def text_to_hex(text):
+    hex_representation = ' '.join(format(ord(char), 'x') for char in text)
+    return hex_representation
+
+
+# YOU KNOW ME VERY WELL...........................
+
+@app.on_message(filters.command("code"))
+def convert_text(_, message):
+    if len(message.command) > 1:
+        input_text = " ".join(message.command[1:])
+
+        hex_representation = text_to_hex(input_text)
+        decoded_text = hex_to_text(input_text)
+
+        response_text = f"𝗜𝗻𝗽𝘂𝘁 𝗧𝗲𝘅𝘁➪\n {input_text}\n\n𝗛𝗲𝘅 𝗥𝗲𝗽𝗿𝗲𝘀𝗲𝗻𝘁𝗮𝘁𝗶𝗼𝗻➪\n {hex_representation}\n\n𝗗𝗲𝗰𝗼𝗱𝗲𝗱 𝗧𝗲𝘅𝘁➪\n {decoded_text}\n\n\n𝗕𝗬 ➪@{BOT_USERNAME}"
+
+        message.reply_text(response_text)
+    else:
+        message.reply_text("Please provide text after the /code command.")
---
+++ ANNIEMUSIC/plugins/Yumi/pypi.py
@@ -0,0 +1,49 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import requests
+from ANNIEMUSIC import app
+
+
+def get_pypi_info(package_name):
+    try:
+        
+        api_url = f"https://pypi.org/pypi/{package_name}/json"
+        
+        # Sending a request to the PyPI API
+        response = requests.get(api_url)
+        
+        # Extracting information from the API response
+        pypi_info = response.json()
+        
+        return pypi_info
+    
+    except Exception as e:
+        print(f"Error fetching PyPI information: {e}")
+        return None
+
+@app.on_message(filters.command("pypi", prefixes="/"))
+def pypi_info_command(client, message):
+    try:
+       
+        package_name = message.command[1]
+        
+        # Getting information from PyPI
+        pypi_info = get_pypi_info(package_name)
+        
+        if pypi_info:
+            # Creating a message with PyPI information
+            info_message = f"ᴘᴀᴄᴋᴀɢᴇ ɴᴀᴍᴇ ➪ {pypi_info['info']['name']}\n\n" \
+                           f"Lᴀᴛᴇsᴛ ᴠɪʀsɪᴏɴ➪ {pypi_info['info']['version']}\n\n" \
+                           f"Dᴇsᴄʀɪᴘᴛɪᴏɴ➪ {pypi_info['info']['summary']}\n\n" \
+                           f"ᴘʀᴏJᴇᴄᴛ ᴜʀʟ➪ {pypi_info['info']['project_urls']['Homepage']}"
+            
+            # Sending the PyPI information back to the user
+            client.send_message(message.chat.id, info_message)
+        
+        else:
+            # Handling the case where information retrieval failed
+            client.send_message(message.chat.id, "Failed to fetch information from PyPI.")
+    
+    except IndexError:
+
+        client.send_message(message.chat.id, "Please provide a package name after the /pypi command.")
