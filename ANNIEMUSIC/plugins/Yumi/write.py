a6a406fbf869d6cc436391350767e72021a0d787
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-15 15:02:30 +0530

    Update write.py

--- ANNIEMUSIC/plugins/Yumi/write.py
+++ ANNIEMUSIC/plugins/Yumi/write.py
@@ -1,8 +1,11 @@
 from pyrogram import filters
+from pyrogram import *
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from config import  BOT_USERNAME
+from datetime import datetime
 from ANNIEMUSIC import app as app
 import requests
+
 
 @app.on_message(filters.command("write"))
 async def handwrite(_, message: Message):
@@ -29,3 +32,24 @@ help = """
 
 ❍ /write <ᴛᴇxᴛ> *:* ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ.
  """
+
+#----------
+
+@app.on_message(filters.command("day"))
+def date_to_day_command(client: Client, message: Message):
+    try:
+        # Extract the date from the command message......
+        command_parts = message.text.split(" ", 1)
+        if len(command_parts) == 2:
+            input_date = command_parts[1].strip()
+            date_object = datetime.strptime(input_date, "%Y-%m-%d")
+            day_of_week = date_object.strftime("%A")
+
+            # Reply with the day of the week
+            message.reply_text(f"The day of the week for {input_date} is {day_of_week}.")
+
+        else:
+            message.reply_text("Please provide a valid date in the format `/day 1947-08-15` ")
+
+    except ValueError as e:
+        message.reply_text(f"Error: {str(e)}")
