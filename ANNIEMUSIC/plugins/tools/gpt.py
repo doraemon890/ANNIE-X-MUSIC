3c1f87be8ddefce13b28c799de34319c9702359e
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-18 23:33:20 +0530

    Update and rename deepai.py to gpt.py

--- ANNIEMUSIC/plugins/tools/gpt.py
+++ ANNIEMUSIC/plugins/tools/gpt.py
@@ -3,17 +3,17 @@ from pyrogram import filters
 from ANNIEMUSIC import app
 from pyrogram.enums import ChatAction, ParseMode
 
-api_key ="995e3267-53f1-496e-82e3-39754eab99dc"
+api_key ="FJnpI0mL2qbcQOU9jSoqT3BlbkFJNLPg9pHWWEUYI4cF9vTQ"
 
 
 
-@app.on_message(filters.command(["deep"],  prefixes=["+", ".", "/"]))
-async def deepchat(app: app, message):
+@app.on_message(filters.command(["jarvis"],  prefixes=["+", ".", "/", ""]))
+async def OpenAIchat(app: app, message):
     name = message.from_user.first_name
     try:
         await app.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
-            await message.reply_text(f"Hello {name}\nPlease provide text after the /deep command.")
+            await message.reply_text(f"Hello {name}\nPlease provide text after the /jarvis command.")
         else:
             a = message.text.split(' ', 1)[1]
 
@@ -25,7 +25,7 @@ async def deepchat(app: app, message):
                 'api-key': api_key,
             }
 
-            r = requests.post("https://api.deepai.org/api/text-generator", data=data, headers=headers)
+            r = requests.post(" https://api.openai.com/v1/chat/completions, data=data, headers=headers)
             response = r.json()
             answer_text = response['output']
             await message.reply_text(f"{answer_text}")
@@ -36,12 +36,12 @@ async def deepchat(app: app, message):
 #####
 
 @app.on_message(filters.command(["aby" , ],  prefixes=["b","B"]))
-async def deepchat(app: app, message):
+async def OpenAIchat(app: app, message):
     name = message.from_user.first_name
     try:
         await app.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
-            await message.reply_text(f"Hello {name}\nPlease provide text after the /deep command.")
+            await message.reply_text(f"Hello {name}\nPlease provide text after the /OpenAI command.")
         else:
             a = message.text.split(' ', 1)[1]
 
@@ -53,7 +53,7 @@ async def deepchat(app: app, message):
                 'api-key': api_key,
             }
 
-            r = requests.post("https://api.deepai.org/api/text-generator", data=data, headers=headers)
+            r = requests.post(" https://api.openai.com/v1/chat/completions, data=data, headers=headers)
             response = r.json()
             answer_text = response['output']
             await message.reply_text(f"{answer_text}")
