a1bef808a461c25b2c9b6ca4c9f23a01364ae1c0
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-19 19:04:56 +0530

    Update gpt.py

--- ANNIEMUSIC/plugins/tools/gpt.py
+++ ANNIEMUSIC/plugins/tools/gpt.py
@@ -5,6 +5,7 @@ from ANNIEMUSIC import app
 from pyrogram.enums import ChatAction, ParseMode
 from gtts import gTTS
 import requests, config
+from config import GPT_API
 import requests as r
 # ----------------------------------------
 openai.api_key = config.GPT_API
