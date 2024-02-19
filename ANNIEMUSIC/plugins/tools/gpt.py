0f82620fc1b97e9d641838000f685bcd52027393
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-19 18:56:59 +0530

    Update gpt.py

--- ANNIEMUSIC/plugins/tools/gpt.py
+++ ANNIEMUSIC/plugins/tools/gpt.py
@@ -7,7 +7,7 @@ from gtts import gTTS
 import requests, config
 import requests as r
 # ----------------------------------------
-openai.api_key = "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"
+openai.api_key = config.GPT_API
 
 API_URL = "https://sugoi-api.vercel.app/search"
 
