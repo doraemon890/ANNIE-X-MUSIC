d4acaad5f2e9ade6838ea4bc3ad1d8dd0dc480ef
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-19 18:37:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,6 +28,8 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", -1002024677280))
 
+# ------------------------------------------------
+GPT_API = int(getenv("GPT_API")
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6788284558))
 
