959771a8ea01d0bf82c85762247d9e1b138314f3
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-19 17:57:02 +0530

    Update annie.py

--- ANNIEMUSIC/plugins/sudo/annie.py
+++ ANNIEMUSIC/plugins/sudo/annie.py
@@ -53,7 +53,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["annie", "Annie"], prefixes=["!", "/", ""]) & admin_filter)
+@app.on_message(filters.command(["nnie"], prefixes=["!", "A", "a"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
