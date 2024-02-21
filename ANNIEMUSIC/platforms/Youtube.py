7b295e1d17cbc71a776c58ab09b4af2234617a44
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-21 15:09:59 +0530

    Update Youtube.py

--- ANNIEMUSIC/platforms/Youtube.py
+++ ANNIEMUSIC/platforms/Youtube.py
@@ -307,7 +307,7 @@ class YouTubeAPI:
                     {
                         "key": "FFmpegExtractAudio",
                         "preferredcodec": "mp3",
-                        "preferredquality": "320",
+                        "preferredquality": "192",
                     }
                 ],
             }
