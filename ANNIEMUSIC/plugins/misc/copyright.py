7dde2d077748cc1371ce3574a575c10f1e67c07d
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-22 22:39:59 +0530

    Update copyright.py

--- ANNIEMUSIC/plugins/misc/copyright.py
+++ ANNIEMUSIC/plugins/misc/copyright.py
@@ -1,10 +1,9 @@
 from ANNIEMUSIC import app
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from ANNIEMUSIC.plugins.misc.CP import CP
 
 
-keywords_to_delete = CP
+keywords_to_delete = ["porn", "xxx", "sex", "NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
 
 async def delete_links(message):
     if any(link in message.text for link in ["http", "https", "www."]):
@@ -33,3 +32,20 @@ async def delete_long_messages(client, message):
 async def delete_edited_long_messages(client, edited_message):
     if len(edited_message.text.split()) >= 20:
         await edited_message.delete()
+
+
+# -------------------------------
+
+async def delete_pdf_files(client, message):
+    if message.document and message.document.mime_type == "application/pdf":
+        warning_message = f"@{message.from_user.username} ᴍᴀᴀ ᴍᴀᴛ ᴄʜᴜᴅᴀ ᴘᴅғ ʙʜᴇᴊ ᴋᴇ,\n ʙʜᴏsᴀᴅɪᴋᴇ ᴄᴏᴘʏʀɪɢʜᴛ ʟᴀɢʏᴇɢᴀ \n\n ᴅᴇʟᴇᴛᴇ ᴋᴀʀ ᴅɪʏᴀ ᴍᴀᴅᴀʀᴄʜᴏᴅ.\n\n ᴀʙ @PRADHAN74 ʙʜᴀɪ ᴋᴇ ᴅᴍ ᴍᴇ ᴀᴘɴɪ ᴍᴜᴍᴍʏ ᴋᴏ ʙʜᴇᴊ ᴅᴇ 🍌🍌🍌."
+        await message.reply_text(warning_message)
+        await message.delete()
+    else:  
+        pass
+
+@app.on_message(filters.group & filters.document)
+async def message_handler(client, message):
+    await delete_pdf_files(client, message)
+
+# -----------------------------------------------------------------------------------
