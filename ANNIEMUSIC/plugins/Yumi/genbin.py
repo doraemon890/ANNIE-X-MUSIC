65f8ac3545b301def346b04f4a9743b84ad6b3fb
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-25 09:52:21 +0530

    Update genbin.py

--- ANNIEMUSIC/plugins/Yumi/genbin.py
+++ ANNIEMUSIC/plugins/Yumi/genbin.py
@@ -1,36 +1,8 @@
-"""MIT License
-
-Copyright (c) [Year] doraemon890
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-#Copyright © [LIFETIME] Team DAXX. All rights reserved
-
-
-
-
 from pyrogram import Client, filters
 import random
 from ANNIEMUSIC import app
 
-# Constants
+
 VALID_PREFIXES = [4, 5, 6,3]  # VISA starts with 4, MasterCard with 5, Discover with 6
 
 
@@ -58,6 +30,6 @@ def generate_test_card_number(prefix, length):
 @app.on_message(filters.command("genbin"))
 def generate(client, message):
     prefix = random.choice(VALID_PREFIXES)
-    length = 6  # Standard credit card length
+    length = 4  # Standard credit card length
     card_number = generate_test_card_number(prefix, length)
-    message.reply_text(f"𝗕𝗜𝗡 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗\n" f"{card_number} ✅")
+    message.reply_text(f"𝗕𝗜𝗡 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗\n" f"`{card_number}` ✅")
