894882618dc6277e8ff9dda2d6e50aad338567d0
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-27 01:21:44 +0530

    Update gmmsg.py

--- ANNIEMUSIC/plugins/tools/gmmsg.py
+++ ANNIEMUSIC/plugins/tools/gmmsg.py
@@ -5,7 +5,7 @@ from ANNIEMUSIC import app
 
 
 ###### GOOOD MORNING 
-@app.on_message(filters.command(["gm","m","oodmorning","ood Morning","ood morning"], prefixes=["/","g","G", "" ]))
+@app.on_message(filters.command(["gm","oodmorning","ood Morning","ood morning"], prefixes=["/","g","G", "" ]))
 def goodnight_command_handler(_, message):
     sender = message.from_user.mention
     send_video = random.choice([True, False])
