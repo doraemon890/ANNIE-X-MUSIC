b9af052b33c8aeddd0c3a496b584b61d9070c3e2
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-11 22:18:05 +0530

    Update ai.py

--- ANNIEMUSIC/plugins/tools/ai.py
+++ ANNIEMUSIC/plugins/tools/ai.py
@@ -1,73 +1,70 @@
-import asyncio,base64,mimetypes,os
+import asyncio
+import base64
+import mimetypes
+import os
 from pyrogram import filters, types as t
 from lexica import AsyncClient
 from ANNIEMUSIC import app
 from lexica.constants import languageModels
 
 
-
-async def ChatCompletion(prompt,model) -> tuple | str :
+async def chat_completion(prompt, model) -> tuple | str:
     try:
-        modelInfo = getattr(languageModels,model)
+        model_info = getattr(languageModels, model)
         client = AsyncClient()
-        output = await client.ChatCompletion(prompt,modelInfo)
+        output = await client.ChatCompletion(prompt, model_info)
         if model == "bard":
             return output['content'], output['images']
         return output['content']
-    except Exception as E:
-        raise Exception(f"API error: {E}",)
+    except Exception as e:
+        raise Exception(f"API error: {e}")
 
 
-
-async def geminiVision(prompt,model,images) -> tuple | str :
-    imageInfo = []
+async def gemini_vision(prompt, model, images) -> tuple | str:
+    image_info = []
     for image in images:
-        with open(image,"rb") as imageFile:
-            data = base64.b64encode(imageFile.read()).decode("utf-8")
-            mime_type,_= mimetypes.guess_type(image)
-            imageInfo.append({
+        with open(image, "rb") as image_file:
+            data = base64.b64encode(image_file.read()).decode("utf-8")
+            mime_type, _ = mimetypes.guess_type(image)
+            image_info.append({
                 "data": data,
                 "mime_type": mime_type
             })
         os.remove(image)
     payload = {
-        "images":imageInfo
+        "images": image_info
     }
-    modelInfo = getattr(languageModels,model)
+    model_info = getattr(languageModels, model)
     client = AsyncClient()
-    output = await client.ChatCompletion(prompt,modelInfo,json=payload)
+    output = await client.ChatCompletion(prompt, model_info, json=payload)
     return output['content']['parts'][0]['text']
 
 
-
-def getMedia(message):
+def get_media(message):
     """Extract Media"""
-    media = message.media if message.media else message.reply_to_message.media if message.reply_to_message else None
+    media = None
     if message.media:
         if message.photo:
             media = message.photo
-        elif message.document and message.document.mime_type in ['image/png','image/jpg','image/jpeg'] and message.document.file_size < 5242880:
+        elif message.document and message.document.mime_type in ['image/png', 'image/jpg', 'image/jpeg'] \
+                and message.document.file_size < 5242880:
             media = message.document
-        else:
-            media = None
     elif message.reply_to_message and message.reply_to_message.media:
         if message.reply_to_message.photo:
             media = message.reply_to_message.photo
-        elif message.reply_to_message.document and message.reply_to_message.document.mime_type in ['image/png','image/jpg','image/jpeg'] and message.reply_to_message.document.file_size < 5242880:
+        elif message.reply_to_message.document and message.reply_to_message.document.mime_type in ['image/png',
+                                                                                                      'image/jpg',
+                                                                                                      'image/jpeg'] \
+                and message.reply_to_message.document.file_size < 5242880:
             media = message.reply_to_message.document
-        else:
-            media = None
-    else:
-        media = None
     return media
 
 
-def getText(message):
+def get_text(message):
     """Extract Text From Commands"""
-    text_to_return = message.text
     if message.text is None:
         return None
-    if " " in text_to_return:
+    if " " in message.text:
         try:
             return message.text.split(None, 1)[1]
         except IndexError:
@@ -76,40 +73,35 @@ def getText(message):
         return None
 
 
-
-
-@app.on_message(filters.command(["bard","gpt","llama","mistral","palm","gemini"]))
-async def chatbots(_,m: t.Message):
-    prompt = getText(m)
-    media = getMedia(m)
+@app.on_message(filters.command(["bard", "gpt", "llama", "mistral", "palm", "gemini"]))
+async def chat_bots(_, m: t.Message):
+    prompt = get_text(m)
+    media = get_media(m)
     if media is not None:
-        return await askAboutImage(_,m,[media],prompt)
+        return await ask_about_image(_, m, [media], prompt)
     if prompt is None:
-        return await m.reply_text("Hello Babes, How can i assist you today?")
+        return await m.reply_text("Hello, how can I assist you today?")
     model = m.command[0].lower()
-    output = await ChatCompletion(prompt,model)
+    output = await chat_completion(prompt, model)
     if model == "bard":
         output, images = output
         if len(images) == 0:
             return await m.reply_text(output)
-        media = []
-        for i in images:
-            media.append(t.InputMediaPhoto(i))
-        media[0] = t.InputMediaPhoto(images[0],caption=output)
+        media = [t.InputMediaPhoto(i) for i in images]
+        media[0] = t.InputMediaPhoto(images[0], caption=output)
         await _.send_media_group(
             m.chat.id,
             media,
-            reply_to_message_id=m.id
-            )
-        return
-    await m.reply_text(output['parts'][0]['text'] if model=="gemini" else output)
+            reply_to_message_id=m.message_id
+        )
+    else:
+        await m.reply_text(output['parts'][0]['text'] if model == "gemini" else output)
 
 
-async def askAboutImage(_,m:t.Message,mediaFiles: list,prompt:str):
+async def ask_about_image(_, m: t.Message, media_files: list, prompt: str):
     images = []
-    for media in mediaFiles:
-        image = await _.download_media(media.file_id,file_name=f'./downloads/{m.from_user.id}_ask.jpg')
+    for media in media_files:
+        image = await _.download_media(media.file_id, file_name=f'./downloads/{m.from_user.id}_ask.jpg')
         images.append(image)
-    output = await geminiVision(prompt if prompt else "whats this?","geminiVision",images)
+    output = await gemini_vision(prompt if prompt else "What's this?", "geminiVision", images)
     await m.reply_text(output)
-
