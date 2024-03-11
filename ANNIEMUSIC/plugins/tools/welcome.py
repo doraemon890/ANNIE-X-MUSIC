d9e524810ce6c0fcc9f2b49510e663a9c89d8b06
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-11 16:21:18 +0530

    Update welcome.py

--- ANNIEMUSIC/plugins/tools/welcome.py
+++ ANNIEMUSIC/plugins/tools/welcome.py
@@ -66,7 +66,7 @@ def circle(pfp, size=(500, 500)):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname):
-    background = Image.open("ANNIEMUSIC/assets/annie/anniewel2.png")
+    background = Image.open("ANNIEMUSIC/assets/annie/anniewel.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize((889, 873))
