c4cfdb19fb6193f3cd36d50ee774402fcc30e0f9
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-15 08:47:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/CDX_WORLD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/JARVIS_X_SUPPORT")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/CHATTING_2024")
 
 
