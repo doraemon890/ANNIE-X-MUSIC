3bb4ada093f12563d7db2108c0e0e06793eb417a
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-18 21:14:48 +0530

    Update urlshortner.py

--- ANNIEMUSIC/plugins/misc/urlshortner.py
+++ ANNIEMUSIC/plugins/misc/urlshortner.py
@@ -82,7 +82,7 @@ async def unshort(bot, message):
 # app.add_handler(MessageHandler(unshort))
 __help__ = """
 ᴍᴀᴋᴇ sʜᴏʀᴛs ᴏғ ᴀ ɢɪᴠᴇɴ ʟɪɴᴋ 
- ❍ /short <url>  *:Example `/short https://t.me/iam_daxx`.
+ ❍ /short <url>  *:Example `/short https://t.me/JARVIS_V2`.
  *"""
 
 __mod_name__ = "Sʜᴏʀᴛᴇɴᴇʀ"
