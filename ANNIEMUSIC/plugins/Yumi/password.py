1b944adc9d31dd2e0175f60580f22e68f593ab42
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-18 21:03:23 +0530

    Update password.py

--- ANNIEMUSIC/plugins/Yumi/password.py
+++ ANNIEMUSIC/plugins/Yumi/password.py
@@ -16,5 +16,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/Annie_X_music_bot?startgroup=true')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
