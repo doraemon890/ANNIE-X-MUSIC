5dbfed73d89304b7fd086576f7447f452effb08b
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-22 00:45:47 +0530

    Update and rename autobroadcast to autobroadcast.py

--- ANNIEMUSIC/plugins/misc/autobroadcast.py
+++ ANNIEMUSIC/plugins/misc/autobroadcast.py
@@ -27,6 +27,8 @@ BUTTON = InlineKeyboardMarkup(
 
 CELEBRATION_VID_URL = "https://github.com/doraemon890/ANNIE-X-MUSIC/assets/155803358/dcbec346-bb98-4818-9a1c-683e9c3107b0"
 
+BD_VID = "https://telegra.ph/file/943bb99829ec526c3f99a.mp4"
+
 async def send_message_to_chats():
     try:
         chats = await get_served_chats()
@@ -35,7 +37,7 @@ async def send_message_to_chats():
             chat_id = chat_info.get('chat_id')
             if isinstance(chat_id, int):  # Check if chat_id is an integer
                 try:
-                    await app.send_photo(chat_id, photo=START_IMG_URL, caption=MESSAGE, reply_markup=BUTTON)
+                    await app.send_video(chat_id, video=BD_VID, caption=MESSAGE, reply_markup=BUTTON)
                     await asyncio.sleep(3)  # Sleep for 1 second between sending messages
                 except Exception as e:
                     pass  # Do nothing if an error occurs while sending message
