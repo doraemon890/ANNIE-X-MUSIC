89c3967d67790cb5504713e5a02b5a93104acb96
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-29 12:22:57 +0530

    Update sudoers.py

--- ANNIEMUSIC/plugins/sudo/sudoers.py
+++ ANNIEMUSIC/plugins/sudo/sudoers.py
@@ -61,7 +61,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝐁𝐞𝐭𝐚 𝐏𝐚𝐡𝐥𝐞 𝐉𝐚𝐫𝐯𝐢𝐬 𝐏𝐚𝐩𝐚 𝐒𝐞 𝐏𝐮𝐜𝐡 𝐋𝐨😁💀", show_alert=True)
+        return await callback_query.answer("ᴘᴀʜʟᴇ ᴊᴀʀᴠɪs ᴘᴀᴘᴀ sᴇ ᴘᴇʀᴍɪssɪᴏɴ ʟᴇʟᴏ😁💀", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
