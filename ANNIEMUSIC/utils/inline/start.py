ea3bd802e3e2d3c8884a592448a0ce120a4a3e0b
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-29 07:41:33 +0530

    Update start.py

--- ANNIEMUSIC/utils/inline/start.py
+++ ANNIEMUSIC/utils/inline/start.py
@@ -8,7 +8,7 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_11"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
         ],
