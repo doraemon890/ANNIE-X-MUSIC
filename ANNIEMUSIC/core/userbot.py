c40bca637e96266b29fd8f9c4c66caa9afb5e0fa
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-06 10:36:05 +0530

    Updation

--- ANNIEMUSIC/core/userbot.py
+++ ANNIEMUSIC/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("loggerV1")
+                await self.one.join_chat("Dora_hub")
               
-                await self.one.join_chat("loggerV2")
+                await self.one.join_chat("loggchk")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("loggerV1")
+                await self.two.join_chat("Dora_hub")
                 
-                await self.one.join_chat("loggerV2")
+                await self.one.join_chat("loggchk")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("loggerV1")
+                await self.three.join_chat("Dora_hub")
                 
-                await self.one.join_chat("loggerV2")
+                await self.one.join_chat("loggchk")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("loggerV1")
+                await self.four.join_chat("Dora_hub")
                 
-                await self.one.join_chat("loggerV2")
+                await self.one.join_chat("loggchk")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("loggerV1")
+                await self.five.join_chat("Dora_hub")
                 
-                await self.one.join_chat("loggerV2")
+                await self.one.join_chat("loggchk")
             except:
                 pass
             assistants.append(5)
