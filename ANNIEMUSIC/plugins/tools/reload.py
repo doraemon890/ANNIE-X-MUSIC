0a67fe6e29562efa252aedf0db4feee1e7114430
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-11 09:47:07 +0530

    Update reload.py

--- ANNIEMUSIC/plugins/tools/reload.py
+++ ANNIEMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7157587567)
+    & filters.user(7044783841)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
