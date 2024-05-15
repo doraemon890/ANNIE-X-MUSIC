bdcb2e79482559e16ee6efbf92e4ba9d0535dd1c
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-15 21:26:52 +0530

    Update notes.py

--- ANNIEMUSIC/plugins/misc/notes.py
+++ ANNIEMUSIC/plugins/misc/notes.py
@@ -5,44 +5,41 @@ from ANNIEMUSIC.utils.jarvis_ban import admin_filter
 from ANNIEMUSIC.mongo.notesdb import *
 from ANNIEMUSIC.utils.notes_func import GetNoteMessage, exceNoteMessageSender, privateNote_and_admin_checker
 from ANNIEMUSIC.utils.yumidb import user_admin
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , Message , CallbackQuery
 from pyrogram.enums import ChatMemberStatus
 
-# Command to save a note
+
 @app.on_message(filters.command("save") & admin_filter)
 @user_admin
-async def save_note(client, message):
+async def _save(client, message):
     chat_id = message.chat.id
     chat_title = message.chat.title
-    if message.reply_to_message and len(message.command) < 2:
-        return await message.reply_text("Please provide a name for the note!")
-
-    if not message.reply_to_message and len(message.command) < 3:
-        return await message.reply_text("Please provide some content for the note!")
-
-    note_name = message.command[1]
-    content, text, data_type = GetNoteMessage(message)
+    if message.reply_to_message and not len(message.command) >= 2:
+        return await message.reply_text("you need to give the note a name!")
     
-    try:
-        await SaveNote(chat_id, note_name, content, text, data_type)
-        await message.reply_text(f"Note '{note_name}' has been saved in {chat_title}.")
-    except Exception as e:
-        await message.reply_text(f"An error occurred while saving the note: {str(e)}")
+    if not message.reply_to_message and not len(message.command) >= 3:
+        return await message.reply_text("You need to give the note some content!")
+    
+    NoteName = message.command[1]
+    Content, Text, DataType = GetNoteMessage(message)
+    await SaveNote(chat_id, NoteName, Content, Text, DataType)
 
-# Command to retrieve a note
+    await message.reply_text(f"I've saved note `{NoteName}` in {chat_title}.")
+    
+
+
+
 @app.on_message(filters.command("get") & admin_filter)
-async def get_note(client, message):
+async def _getnote(client, message):
     chat_id = message.chat.id
-    if len(message.command) < 2:
-        return await message.reply_text("Please specify the name of the note!")
-
+    if not len(message.command) >= 2:
+        return await message.reply_text("You need to give the note a name!")  
     note_name = message.command[1]
     if not await isNoteExist(chat_id, note_name):
-        return await message.reply_text("Note not found")
-
+         return await message.reply_text("Note not found")
     await send_note(message, note_name)
+    
 
-# Regular expression to retrieve a note
 @app.on_message(filters.regex(pattern=(r"^#[^\s]+")) & filters.group)
 async def regex_get_note(client, message):
     chat_id = message.chat.id
@@ -51,110 +48,193 @@ async def regex_get_note(client, message):
         if await isNoteExist(chat_id, note_name):
             await send_note(message, note_name)
 
-# Command to toggle private notes setting
+
+PRIVATE_NOTES_TRUE = ['on', 'true', 'yes', 'y']
+PRIVATE_NOTES_FALSE = ['off', 'false', 'no', 'n']
+
 @app.on_message(filters.command("privatenotes") & filters.group)
 @user_admin
-async def private_notes(client, message):
+async def PrivateNote(client, message):
     chat_id = message.chat.id
-    if len(message.command) < 2:
-        return await message.reply_text("Please specify 'on' or 'off'!")
+    if len(message.command) >= 2:
+        if (
+            message.command[1] in PRIVATE_NOTES_TRUE
+        ):
+            await set_private_note(chat_id, True)
+            await message.reply(
+                "Now i will send a message to your chat with a button redirecting to PM, where the user will receive the note.",
+                quote=True
+            )
 
-    setting = message.command[1].lower()
-    if setting in ['on', 'off']:
-        await set_private_note(chat_id, setting == 'on')
-        await message.reply_text(f"Private notes setting updated to '{setting}'.")
+        elif (
+            message.command[1] in PRIVATE_NOTES_FALSE
+        ):
+            await set_private_note(chat_id, False)
+            await message.reply(
+                "I will now send notes straight to the group.",
+                quote=True
+            )  
+        else:
+            await message.reply(
+                f"failed to get boolean value from input:\n\n expected one of y/yes/on/true or n/no/off/false; got: {message.command[1]}",
+                quote=True
+            )
     else:
-        await message.reply_text("Invalid setting. Please specify 'on' or 'off'!")
-
-# Command to clear a specific note
+        if await is_pnote_on(chat_id):
+            await message.reply(
+                "Your notes are currently being sent in private. ANNIEMUSIC will send a small note with a button which redirects to a private chat.",
+                quote=True
+            )
+        else:
+            await message.reply(
+                "Your notes are currently being sent in the group.",
+                quote=True
+            )
+            
 @app.on_message(filters.command("clear") & admin_filter)
 @user_admin
-async def clear_note(client, message):
-    chat_id = message.chat.id
-    if len(message.command) < 2:
-        return await message.reply_text("Please specify the name of the note!")
-
+async def Clear_Note(client, message):
+    chat_id = message.chat.id 
+    if not (
+        len(message.command) >= 2
+    ):
+        await message.reply(
+            "You need to give the note a name!",
+            quote=True
+        )
+        return
+    
     note_name = message.command[1].lower()
+
     if await isNoteExist(chat_id, note_name):
         await ClearNote(chat_id, note_name)
-        await message.reply_text(f"Note '{note_name}' has been deleted.")
-    else:
-        await message.reply_text("Note not found.")
 
-# Command to clear all notes
+        await message.reply(
+            f"I've removed the note `{note_name}`!.",
+            quote=True
+        )
+    else:
+        await message.reply(
+            "You haven't saved a note with this name yet!",
+            quote=True
+        )
+
+
 @app.on_message(filters.command("clearall") & admin_filter)
-async def clear_all_notes(client, message):
+async def ClearAll_Note(client, message):
     owner_id = message.from_user.id
-    chat_id = message.chat.id
+    chat_id = message.chat.id 
     chat_title = message.chat.title
-    user = await client.get_chat_member(chat_id, owner_id)
-    if user.status != ChatMemberStatus.OWNER:
-        return await message.reply_text("Only the owner can use this command!")
+    user = await client.get_chat_member(chat_id,owner_id)
+    if not user.status == ChatMemberStatus.OWNER :
+        return await message.reply_text("Only Owner Can Use This!!") 
 
     note_list = await NoteList(chat_id)
-    if not note_list:
-        return await message.reply_text(f"No notes found in {chat_title}.")
-
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton(text='Delete all notes', callback_data=f'clearallnotes_clear_{owner_id}_{chat_id}')],
-        [InlineKeyboardButton(text='Cancel', callback_data=f'clearallnotes_cancel_{owner_id}')]
-    ])
-    await message.reply_text(
-        f"Are you sure you want to delete **ALL** notes in {chat_title}? This action is irreversible.",
-        reply_markup=keyboard
+    if note_list == 0:
+        await message.reply(
+            f"No notes in {chat_title}",
+            quote=True
+        )
+    keyboard = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton(text='Delete all notes', callback_data=f'clearallnotes_clear_{owner_id}_{chat_id}')
+        ],
+        [
+            InlineKeyboardButton(text='Cancel', callback_data=f'clearallnotes_cancel_{owner_id}')
+        ]]
+    )
+    await message.reply(
+        f"Are you sure you want to clear **ALL** notes in {chat_title}? This action is irreversible.",
+        reply_markup=keyboard,
+        quote=True
     )
 
-# Callback query handler for clearing all notes
 @app.on_callback_query(filters.regex("^clearallnotes_"))
-async def clear_all_callback(client, callback_query: CallbackQuery):
+async def ClearAllCallback(client, callback_query: CallbackQuery):
     query_data = callback_query.data.split('_')[1]
     owner_id = int(callback_query.data.split('_')[2])
-    user_id = callback_query.from_user.id
+    user_id = callback_query.from_user.id 
 
     if owner_id == user_id:
         if query_data == 'clear':
             chat_id = int(callback_query.data.split('_')[3])
             await ClearAllNotes(chat_id)
-            await callback_query.answer("All notes have been deleted.")
+            await callback_query.edit_message_text("Deleted all chat notes.") 
+            return
+            
         elif query_data == 'cancel':
-            await callback_query.answer("Cancelled.")
+            await callback_query.edit_message_text("Cancelled.")
     else:
         await callback_query.answer("Only admins can execute this command!")
-
-# Command to list all saved notes
+                         
 @app.on_message(filters.command(['notes', 'saved']) & filters.group)
-async def list_notes(client, message):
+async def Notes(client, message):
+    
     chat_id = message.chat.id
     chat_title = message.chat.title
-    notes_list = await NoteList(chat_id)
-    if notes_list:
-        note_header = f"List of notes in {chat_title}:\n"
-        note_list_str = '\n'.join([f" • `#{note}`" for note in notes_list])
-        await message.reply_text(
-            f"{note_header}{note_list_str}\nYou can retrieve these notes using `/get notename` or `#notename`.",
+
+    Notes_list = await NoteList(chat_id)
+    
+    NoteHeader = f"List of notes  in {chat_title}:\n"
+    if (
+        len(Notes_list) != 0
+    ): 
+        for notes in Notes_list:
+            NoteName = f" • `#{notes}`\n"
+            NoteHeader += NoteName
+        await message.reply(
+            (
+                f"{NoteHeader}\n"
+                "You can retrieve these notes by using `/get notename`, or `#notename`"
+            ),
             quote=True
         )
+        
     else:
-        await message.reply_text(f"No notes found in {chat_title}.", quote=True)
+        await message.reply(
+            f"No notes in {chat_title}.",
+            quote=True
+        )        
 
-# Function to send a note message
-async def send_note(message, note_name):
-    chat_id = message.chat.id
+async def send_note(message: Message, note_name: str):
+    chat_id = message.chat.id  
     content, text, data_type = await GetNote(chat_id, note_name)
-    private_note, allow = await privateNote_and_admin_checker(message, text)
+    privateNote, allow = await privateNote_and_admin_checker(message, text)   
     if allow:
-        if private_note is None or private_note == await is_pnote_on(chat_id):
-            if private_note:
-                await private_note_button(message, chat_id, note_name)
+        if privateNote == None:
+            if await is_pnote_on(chat_id):
+                await PrivateNoteButton(message, chat_id, note_name)
             else:
                 await exceNoteMessageSender(message, note_name)
+            
+        elif privateNote is not None:
+            if await is_pnote_on(chat_id):
+                if privateNote:
+                    await PrivateNoteButton(message, chat_id, note_name)
+                else:
+                    await exceNoteMessageSender(message, note_name)
+            else: 
+                if privateNote:
+                    await PrivateNoteButton(message, chat_id, note_name)
+                else:
+                    await exceNoteMessageSender(message, note_name)
+                    
+                    
 
-# Function to send a private note button
-async def private_note_button(message, chat_id, note_name):
-    private_note_button = InlineKeyboardMarkup([
-        [InlineKeyboardButton(text='Click me!', url=f'http://t.me/{BOT_USERNAME}?start=note_{chat_id}_{note_name}')]
-    ])
-    await message.reply_text(
-        text=f"Tap here to view '{note_name}' in your private chat.",
-        reply_markup=private_note_button
+async def note_redirect(message):
+    chat_id = int(message.command[1].split('_')[1])
+    note_name = message.command[1].split('_')[2]
+    await exceNoteMessageSender(message, note_name, from_chat_id=chat_id)
+
+async def PrivateNoteButton(message, chat_id, NoteName):
+    PrivateNoteButton = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text='Click me!', url=f'http://t.me/{BOT_USERNAME}?start=note_{chat_id}_{NoteName}')
+            ]
+        ]
+    )
+    await message.reply(
+        text=f"Tap here to view '{NoteName}' in your private chat.",
+        reply_markup=PrivateNoteButton
     )
