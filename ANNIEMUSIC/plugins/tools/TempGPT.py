b66f1dee369df788a935860ea85c7b76ca5bf1af
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-21 15:16:02 +0530

    Update TempGPT.py

--- ANNIEMUSIC/plugins/tools/TempGPT.py
+++ ANNIEMUSIC/plugins/tools/TempGPT.py
@@ -15,7 +15,7 @@ async def chat_gpt(bot, message):
 
         if len(message.command) < 2:
             await message.reply_text(
-                "**Hello {name}, I am Jarvis. How can I help you today?**"
+                "**Hello Sir, I am Jarvis. How can I help you today?**"
             )
         else:
             a = message.text.split(' ', 1)[1]
