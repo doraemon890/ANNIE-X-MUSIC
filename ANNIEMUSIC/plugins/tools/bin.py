c96bef3c672d03cb4c620e8c8806d936a781bc27
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-21 20:20:27 +0530

    Update bin.py

--- ANNIEMUSIC/plugins/tools/bin.py
+++ ANNIEMUSIC/plugins/tools/bin.py
@@ -1,7 +1,7 @@
-import requests
+from ... import *
 from pyrogram import *
 from pyrogram.types import *
-from ANNIEMUSIC import app
+
 
 @app.on_message(filters.command(["bin", "ccbin", "bininfo"], [".", "!", "/"]))
 async def check_ccbin(client, message):
@@ -17,38 +17,24 @@ async def check_ccbin(client, message):
     bin = message.text.split(None, 1)[1]
     if len(bin) < 6:
         return await aux.edit("<b>❌ Wrong Bin❗...</b>")
-    
-    url = "https://bin-ip-checker.p.rapidapi.com/"
-    querystring = {"bin": bin}
-    headers = {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": "693ecdde8emshdf345b92e75f9f7p1b7768jsn73d829ff3e5b",
-        "X-RapidAPI-Host": "bin-ip-checker.p.rapidapi.com"
-    }
-    
     try:
-        response = requests.post(url, headers=headers, params=querystring)
-        data = response.json()
-        
-        if data.get("success", False):
-            bin_info = data.get("BIN", {})
-            await aux.edit(f"""
+        resp = await api.bininfo(bin)
+        await aux.edit(f"""
 <b> 𝗩𝗔𝗟𝗜𝗗 𝗕𝗜𝗡 ✅</b>
 
 <b>┏━◆</b>
-<b>┣〖🏦 ʙᴀɴᴋ</b> ⇾<tt>{bin_info.get('issuer', {}).get('name', 'N/A')}</tt>
-<b>┣〖💳 ʙɪɴ</b> ⇾<tt>`{bin}`</tt>
-<b>┣〖🏡 ᴄᴏᴜɴᴛʀʏ</b> ⇾<tt>{bin_info.get('country', {}).get('country', 'N/A')}</tt>
-<b>┣〖🇮🇳 ғʟᴀɢ</b> ⇾<tt>{bin_info.get('country', {}).get('alpha2', 'N/A')}</tt>
-<b>┣〖🧿 ɪsᴏ</b> ⇾<tt>{bin_info.get('country', {}).get('alpha3', 'N/A')}</tt>
-<b>┣〖⏳ ʟᴇᴠᴇʟ</b> ⇾<tt>{bin_info.get('level', 'N/A')}</tt>
-<b>┣〖🔴 ᴘʀᴇᴘᴀɪᴅ</b> ⇾<tt>{'Yes' if bin_info.get('type') == 'DEBIT' else 'No'}</tt>
-<b>┣〖🆔 ᴛʏᴘᴇ</b> ⇾<tt>{bin_info.get('type', 'N/A')}</tt>
-<b>┣〖ℹ️ ᴠᴇɴᴅᴏʀ</b> ⇾<tt>{bin_info.get('brand', 'N/A')}</tt>
+<b>┣〖🏦 ʙᴀɴᴋ</b> ⇾<tt>{resp.bank}</tt>
+<b>┣〖💳 ʙɪɴ</b> ⇾<tt>`{resp.bin}`</tt>
+<b>┣〖🏡 ᴄᴏᴜɴᴛʀʏ</b> ⇾<tt>{resp.country}</tt>
+<b>┣〖🇮🇳 ғʟᴀɢ</b> ⇾<tt>{resp.flag}</tt>
+<b>┣〖🧿 ɪsᴏ</b> ⇾<tt>{resp.iso}</tt>
+<b>┣〖⏳ ʟᴇᴠᴇʟ</b> ⇾<tt>{resp.level}</tt>
+<b>┣〖🔴 ᴘʀᴇᴘᴀɪᴅ</b> ⇾<tt>{resp.prepaid}</tt>
+<b>┣〖🆔 ᴛʏᴘᴇ</b> ⇾<tt>{resp.type}</tt>
+<b>┣〖ℹ️ ᴠᴇɴᴅᴏʀ</b> ⇾<tt>{resp.vendor}</tt>
 <b>┗━━━◆</b>
-""")
-        else:
-            await aux.edit("🚫 BIN not recognized. Please enter a valid BIN.")
-    except Exception as e:
-        print(e)
-        await aux.edit("❌ An error occurred while fetching BIN information.")
+"""
+        )
+    except:
+        return await aux.edit(f"""
+🚫 BIN not recognized. Please enter a valid BIN.""")
