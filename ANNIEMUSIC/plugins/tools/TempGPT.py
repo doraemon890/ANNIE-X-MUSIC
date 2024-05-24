5e3928e69312abae5a700f20a15b7e822f3ba8bf
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-24 16:40:47 +0530

    Update TempGPT.py

--- ANNIEMUSIC/plugins/tools/TempGPT.py
+++ ANNIEMUSIC/plugins/tools/TempGPT.py
@@ -7,7 +7,7 @@ from config import BOT_USERNAME
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
-@app.on_message(filters.command(["chatgpt","iri"],  prefixes=[".", "/", "S", "s"]))
+@app.on_message(filters.command(["chatgpt","arvis"],  prefixes=[".", "/", "J", "j"]))
 async def chat_gpt(bot, message):
     try:
         start_time = time.time()
