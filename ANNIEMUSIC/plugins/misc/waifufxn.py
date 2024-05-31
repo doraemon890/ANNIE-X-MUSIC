07de852621b4e3bb00c0c5696411b771fe059f59
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-31 12:04:22 +0530

    Update waifufxn.py

--- ANNIEMUSIC/plugins/misc/waifufxn.py
+++ ANNIEMUSIC/plugins/misc/waifufxn.py
@@ -42,7 +42,7 @@ def animation_command(client, message):
         }
 
         command = message.command[0].lower()
-        api_token = "MTI0MTI5MDYxNTcwMzA4MTAxMQ--.MTcxNjAxNzI2Mw--.63190ae1e0"
+        api_token = "MTI0NTIzMTA5NDYxMDEzNzEyMA--.MTcxNzEzNzE3OA--.c69a207b0"
         gif_url = get_animation(api_token, command)
 
         if gif_url:
