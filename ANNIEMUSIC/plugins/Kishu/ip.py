17e95a8f1c4ba1d982cf0f85553e40cb10343623
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-04 16:37:54 +0530

    Update ip.py

--- ANNIEMUSIC/plugins/Kishu/ip.py
+++ ANNIEMUSIC/plugins/Kishu/ip.py
@@ -2,8 +2,6 @@ from pyrogram import Client, filters
 import requests
 from ANNIEMUSIC import app
 
-############.....
-
 @app.on_message(filters.command(["ip"]))
 def ip_info(_, message):
     if len(message.command) != 2:
@@ -18,14 +16,23 @@ def ip_info(_, message):
     else:
         message.reply_text("Unable to fetch information for the provided IP address.")
 
-
 def get_ip_info(ip_address):
-    api_url = f"https://api.safone.dev/ipinfo?ip={ip_address}"
+    api_url = f"https://ipinfo.io/{ip_address}?token=434e1cea389a93"
     try:
         response = requests.get(api_url)
         if response.status_code == 200:
             data = response.json()
-            info = f"IP: {data['ip']}\nCountry: {data['country']}\nCity: {data['city']}\nISP: {data['isp']}"
+            info = (
+                f"IP: {data.get('ip', 'N/A')}\n"
+                f"City: {data.get('city', 'N/A')}\n"
+                f"Region: {data.get('region', 'N/A')}\n"
+                f"Country: {data.get('country', 'N/A')}\n"
+                f"Location: {data.get('loc', 'N/A')}\n"
+                f"Organization: {data.get('org', 'N/A')}\n"
+                f"Postal Code: {data.get('postal', 'N/A')}\n"
+                f"Timezone: {data.get('timezone', 'N/A')}"
+            )
             return info
     except Exception as e:
         print(f"Error fetching IP information: {e}")
+    return None
