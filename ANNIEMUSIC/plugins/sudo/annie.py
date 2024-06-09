f04e2e66b187ff4d9ea408d3cef531fb3808c730
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-09 21:26:01 +0530

    Update annie.py

--- ANNIEMUSIC/plugins/sudo/annie.py
+++ ANNIEMUSIC/plugins/sudo/annie.py
@@ -2,41 +2,34 @@ import requests
 import random
 from ANNIEMUSIC import app, userbot
 from ANNIEMUSIC.misc import SUDOERS
-from pyrogram import * 
+from pyrogram import *
 from pyrogram.types import *
 from ANNIEMUSIC.utils.jarvis_ban import admin_filter
 
-
-
-
-
-
 Jarvis_text = [
-"hey please don't disturb me.",
-"who are you",    
-"aap kon ho",
-"aap mere owner to nhi lgte ",
-"hey tum mera name kyu le rhe ho meko sone do",
-"ha bolo kya kaam hai ",
-"dekho abhi mai busy hu ",
-"hey i am busy",
-"aapko smj nhi aata kya ",
-"leave me alone",
-"dude what happend",    
+    "hey please don't disturb me.",
+    "who are you",    
+    "aap kon ho",
+    "aap mere owner to nhi lgte ",
+    "hey tum mera name kyu le rhe ho meko sone do",
+    "ha bolo kya kaam hai ",
+    "dekho abhi mai busy hu ",
+    "hey i am busy",
+    "aapko smj nhi aata kya ",
+    "leave me alone",
+    "dude what happend",    
 ]
 
 strict_txt = [
-"i can't restrict against my besties",
-"are you serious i am not restrict to my friends",
-"fuck you bsdk k mai apne dosto ko kyu kru",
-"hey stupid admin ", 
-"ha ye phele krlo maar lo ek dusre ki gwaand",  
-"i can't hi is my closest friend",
-"i love him please don't restict this user try to usertand "
+    "i can't restrict against my besties",
+    "are you serious i am not restrict to my friends",
+    "fuck you bsdk k mai apne dosto ko kyu kru",
+    "hey stupid admin ", 
+    "ha ye phele krlo maar lo ek dusre ki gwaand",  
+    "i can't hi is my closest friend",
+    "i love him please don't restict this user try to usertand "
 ]
 
-
- 
 ban = ["ban","boom"]
 unban = ["unban",]
 mute = ["mute","silent","shut"]
@@ -45,16 +38,12 @@ kick = ["kick", "out","nikaal","nikal"]
 promote = ["promote","adminship"]
 fullpromote = ["fullpromote","fulladmin"]
 demote = ["demote","lelo"]
-group = ["group"]
-channel = ["channel"]
-
-
-
-# ========================================= #
-
+lock = ["lock"]
+unlock = ["unlock"]
+lockable_content = ["text", "video", "audio", "photo", "document", "sticker"]
 
 @app.on_message(filters.command(["nnie"], prefixes=["A", "a"]) & admin_filter)
-async def restriction_app(app :app, message):
+async def restriction_app(app: app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
     if len(message.text) < 2:
@@ -84,7 +73,6 @@ async def restriction_app(app :app, message):
             if kicked in kick:
                 if user_id in SUDOERS:
                     await message.reply(random.choice(strict_txt))
-                
                 else:
                     await app.ban_chat_member(chat_id, user_id)
                     await app.unban_chat_member(chat_id, user_id)
@@ -95,7 +83,6 @@ async def restriction_app(app :app, message):
             if muted in mute:
                 if user_id in SUDOERS:
                     await message.reply(random.choice(strict_txt))
-                
                 else:
                     permissions = ChatPermissions(can_send_messages=False)
                     await message.chat.restrict_member(user_id, permissions)
@@ -107,7 +94,6 @@ async def restriction_app(app :app, message):
                 permissions = ChatPermissions(can_send_messages=True)
                 await message.chat.restrict_member(user_id, permissions)
                 await message.reply(f"Huh, OK, sir!")   
-
 
         for promoted in data:
             print(f"present {promoted}")            
@@ -121,8 +107,7 @@ async def restriction_app(app :app, message):
                     can_promote_members=False,
                     can_manage_chat=True,
                     can_manage_video_chats=True,
-                       )
-                     )
+                ))
                 await message.reply("promoted !")
 
         for demoted in data:
@@ -137,24 +122,57 @@ async def restriction_app(app :app, message):
                     can_promote_members=False,
                     can_manage_chat=False,
                     can_manage_video_chats=False,
-                       )
-                     )
+                ))
                 await message.reply("demoted !")
 
+        for fullpromoted in data:
+            print(f"present {fullpromoted}")            
+            if fullpromoted in fullpromote:
+                await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
+                    can_change_info=True,
+                    can_invite_users=True,
+                    can_delete_messages=True,
+                    can_restrict_members=True,
+                    can_pin_messages=True,
+                    can_promote_members=True,
+                    can_manage_chat=True,
+                    can_manage_video_chats=True,
+                ))
+                await message.reply("fullpromoted !")
+                
+        for lock_action in data:
+            print(f"present {lock_action}")
+            if lock_action in lock:
+                if "text" in data:
+                    permissions = ChatPermissions(can_send_messages=False)
+                elif "video" in data:
+                    permissions = ChatPermissions(can_send_media_messages=False)
+                elif "audio" in data:
+                    permissions = ChatPermissions(can_send_other_messages=False)
+                elif "photo" in data:
+                    permissions = ChatPermissions(can_send_photos=False)
+                elif "document" in data:
+                    permissions = ChatPermissions(can_send_documents=False)
+                elif "sticker" in data:
+                    permissions = ChatPermissions(can_send_stickers=False)
+                await app.set_chat_permissions(chat_id, permissions)
+                await message.reply(f"Locked {data[2]} content!")
 
-#async def your_function():
-    for fullpromoted in data:
-        print(f"present {fullpromoted}")            
-        if fullpromoted in fullpromote:
-            await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
-                can_change_info=True,
-                can_invite_users=True,
-                can_delete_messages=True,
-                can_restrict_members=True,
-                can_pin_messages=True,
-                can_promote_members=True,
-                can_manage_chat=True,
-                can_manage_video_chats=True,
-               )
-             )
-            await message.reply("fullpromoted !")
+        for unlock_action in data:
+            print(f"present {unlock_action}")
+            if unlock_action in unlock:
+                if "text" in data:
+                    permissions = ChatPermissions(can_send_messages=True)
+                elif "video" in data:
+                    permissions = ChatPermissions(can_send_media_messages=True)
+                elif "audio" in data:
+                    permissions = ChatPermissions(can_send_other_messages=True)
+                elif "photo" in data:
+                    permissions = ChatPermissions(can_send_photos=True)
+                elif "document" in data:
+                    permissions = ChatPermissions(can_send_documents=True)
+                elif "sticker" in data:
+                    permissions = ChatPermissions(can_send_stickers=True)
+                await app.set_chat_permissions(chat_id, permissions)
+                await message.reply(f"Unlocked {data[2]} content!")
+
