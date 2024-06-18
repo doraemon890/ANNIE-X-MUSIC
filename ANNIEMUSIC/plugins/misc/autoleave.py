e5261be3cdf6a91d5c9d25f4e7d9054d9cd1d4d6
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-18 13:45:12 +0530

    Update autoleave.py

--- ANNIEMUSIC/plugins/misc/autoleave.py
+++ ANNIEMUSIC/plugins/misc/autoleave.py
@@ -10,38 +10,7 @@ from ANNIEMUSIC.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(900):
-            from ANNIEMUSIC.core.userbot import assistants
-
-            for num in assistants:
-                client = await get_client(num)
-                left = 0
-                try:
-                    async for i in client.get_dialogs():
-                        if i.chat.type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
-                        ]:
-                            if (
-                                i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002055415055
-                                and i.chat.id != -1002084489986
-                            ):
-                                if left == 20:
-                                    continue
-                                if not await is_active_chat(i.chat.id):
-                                    try:
-                                        await client.leave_chat(i.chat.id)
-                                        left += 1
-                                    except:
-                                        continue
-                except:
-                    pass
-
-
-asyncio.create_task(auto_leave())
+    pass  # This function does nothing, effectively disabling the auto leave assistant feature
 
 
 async def auto_end():
@@ -71,4 +40,5 @@ async def auto_end():
                     continue
 
 
+asyncio.create_task(auto_leave())
 asyncio.create_task(auto_end())
