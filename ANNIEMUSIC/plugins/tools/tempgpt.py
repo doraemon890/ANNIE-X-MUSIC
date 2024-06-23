a44b916f87da1eb8fe06d79a14bfb9df010ed80d
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-23 11:13:32 -0700

    Update tempgpt.py

--- ANNIEMUSIC/plugins/tools/tempgpt.py
+++ ANNIEMUSIC/plugins/tools/tempgpt.py
@@ -1,40 +1,23 @@
-import random
-import time
 import requests
 from ANNIEMUSIC import app
-from config import BOT_USERNAME
-
+import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
+from MukeshAPI import api
 
-@app.on_message(filters.command(["arvis"],  prefixes=[".", "/", "Z", "z"]))
+@app.on_message(filters.command(["chatgpt", "ai", "ask", "arvis", "iri"], prefixes=[".", "J", "j", "s", "S", "/"]))
 async def chat_gpt(bot, message):
     try:
-        start_time = time.time()
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-
+        
+        # Check if name is defined, if not, set a default value
+        name = message.from_user.first_name if message.from_user else "User"
+        
         if len(message.command) < 2:
-            await message.reply_text(
-                "**Hello Sir, I am Jarvis. How can I help you today?**"
-            )
+            await message.reply_text(f"**Hello {name}, How can I help you today?**")
         else:
-            a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
-
-            try:
-                # Check if "results" key is present in the JSON response
-                if "answer" in response.json():
-                    x = response.json()["answer"]
-                    end_time = time.time()
-                    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
-                    await message.reply_text(
-                        f" {x} ",
-                        parse_mode=ParseMode.MARKDOWN
-                    )
-                else:
-                    await message.reply_text("No 'results' key found in the response.")
-            except KeyError:
-                # Handle any other KeyError that might occur
-                await message.reply_text("Error accessing the response.")
+            query = message.text.split(' ', 1)[1]
+            response = api.gemini(query)["results"]
+            await message.reply_text(f"{response}", parse_mode=ParseMode.MARKDOWN)
     except Exception as e:
-        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+        await message.reply_text(f"**Error: {e}**")
