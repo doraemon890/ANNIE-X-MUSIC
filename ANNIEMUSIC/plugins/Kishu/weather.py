0e3f8930c24d956e16c14c75b20739edb7d84198
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-24 02:32:24 -0700

    Update weather.py

--- ANNIEMUSIC/plugins/Kishu/weather.py
+++ ANNIEMUSIC/plugins/Kishu/weather.py
@@ -1,17 +1,70 @@
-from pyrogram import Client, filters
+import httpx
+from pyrogram import Client
+from pyrogram.types import Message
+
 from ANNIEMUSIC import app
+
+timeout = httpx.Timeout(40, pool=None)
+
+http = httpx.AsyncClient(http2=True, timeout=timeout)
+
+
+# Api key used in weather.com's mobile app.
+weather_apikey = "8de2d8b3a93542c9a2d8b3a935a2c909"
+
+get_coords = "https://api.weather.com/v3/location/search"
+url = "https://api.weather.com/v3/aggcommon/v3-wx-observations-current"
+
+headers = {
+    "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 12; M2012K11AG Build/SQ1D.211205.017)"
+}
 
 
 @app.on_message(filters.command("weather"))
-def weather(client, message):
-    try:
-        # Get the location from user message
-        user_input = message.command[1]
-        location = user_input.strip()
-        weather_url = f"https://wttr.in/{location}.png"
-        
-        # Reply with the weather information as a photo
-        message.reply_photo(photo=weather_url, caption="Here's the weather for your location")
-    except IndexError:
-        # User didn't provide a location
-        message.reply_text("Please provide a location. Use /weather NEW YORK")
+async def weather(c: Client, m: Message):
+    if len(m.command) == 1:
+        return await m.reply_text(
+            "<b>ᴜsᴀɢᴇ:</b> <code>/weather location ᴏʀ city</code> - ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴡᴇᴀᴛʜᴇʀ ɪɴ <i>ʟᴏᴄᴀᴛɪᴏɴ ᴏʀ ᴄɪᴛʏ</i>"
+        )
+
+    r = await http.get(
+        get_coords,
+        headers=headers,
+        params=dict(
+            apiKey=weather_apikey,
+            format="json",
+            language="en",
+            query=m.text.split(maxsplit=1)[1],
+        ),
+    )
+    loc_json = r.json()
+
+    if not loc_json.get("location"):
+        await m.reply_text("ʟᴏᴄᴀᴛɪᴏɴ ɴᴏᴛ ғᴏᴜɴᴅ")
+    else:
+        pos = f"{loc_json['location']['latitude'][0]},{loc_json['location']['longitude'][0]}"
+        r = await http.get(
+            url,
+            headers=headers,
+            params=dict(
+                apiKey=weather_apikey,
+                format="json",
+                language="en",
+                geocode=pos,
+                units="m",
+            ),
+        )
+        res_json = r.json()
+
+        obs_dict = res_json["v3-wx-observations-current"]
+
+        res = "<b>{location}</b>:\n\nᴛᴇᴍᴘᴇʀᴀᴛᴜʀᴇ: <code>{temperature} °C</code>\nᴛᴇᴍᴘᴇʀᴀᴛᴜʀᴇ ғᴇᴇʟs ʟɪᴋᴇ: <code>{feels_like} °C</code>\nᴀɪʀ ʜᴜᴍɪᴅɪᴛʏ: <code>{air_humidity}%</code>\nᴡɪɴᴅ sᴘᴇᴇᴅ: <code>{wind_speed} km/h</code>\n\n- <i>{overview}</i>".format(
+            location=loc_json["location"]["address"][0],
+            temperature=obs_dict["temperature"],
+            feels_like=obs_dict["temperatureFeelsLike"],
+            air_humidity=obs_dict["relativeHumidity"],
+            wind_speed=obs_dict["windSpeed"],
+            overview=obs_dict["wxPhraseLong"],
+        )
+
+        await m.reply_text(res)
