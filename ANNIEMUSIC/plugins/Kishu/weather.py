dff07f0efa983c20d27d730323c1b37ea4071ed1
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-24 02:49:11 -0700

    Update weather.py

--- ANNIEMUSIC/plugins/Kishu/weather.py
+++ ANNIEMUSIC/plugins/Kishu/weather.py
@@ -1,13 +1,10 @@
 import httpx
-from pyrogram import Client
+from pyrogram import Client, filters
 from pyrogram.types import Message
-
 from ANNIEMUSIC import app
 
 timeout = httpx.Timeout(40, pool=None)
-
 http = httpx.AsyncClient(http2=True, timeout=timeout)
-
 
 # Api key used in weather.com's mobile app.
 weather_apikey = "8de2d8b3a93542c9a2d8b3a935a2c909"
@@ -18,7 +15,6 @@ url = "https://api.weather.com/v3/aggcommon/v3-wx-observations-current"
 headers = {
     "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 12; M2012K11AG Build/SQ1D.211205.017)"
 }
-
 
 @app.on_message(filters.command("weather"))
 async def weather(c: Client, m: Message):
@@ -40,7 +36,7 @@ async def weather(c: Client, m: Message):
     loc_json = r.json()
 
     if not loc_json.get("location"):
-        await m.reply_text("ʟᴏᴄᴀᴛɪᴏɴ ɴᴏᴛ ғᴏᴜɴᴅ")
+        await m.reply_text("Location not found")
     else:
         pos = f"{loc_json['location']['latitude'][0]},{loc_json['location']['longitude'][0]}"
         r = await http.get(
@@ -58,7 +54,14 @@ async def weather(c: Client, m: Message):
 
         obs_dict = res_json["v3-wx-observations-current"]
 
-        res = "<b>{location}</b>:\n\nᴛᴇᴍᴘᴇʀᴀᴛᴜʀᴇ: <code>{temperature} °C</code>\nᴛᴇᴍᴘᴇʀᴀᴛᴜʀᴇ ғᴇᴇʟs ʟɪᴋᴇ: <code>{feels_like} °C</code>\nᴀɪʀ ʜᴜᴍɪᴅɪᴛʏ: <code>{air_humidity}%</code>\nᴡɪɴᴅ sᴘᴇᴇᴅ: <code>{wind_speed} km/h</code>\n\n- <i>{overview}</i>".format(
+        res = (
+            "<b>{location}</b>:\n\n"
+            "ᴛᴇᴍᴘᴇʀᴀᴛᴜʀᴇ: <code>{temperature} °C</code>\n"
+            "ᴛᴇᴍᴘᴇʀᴀᴛᴜʀᴇ ғᴇᴇʟs ʟɪᴋᴇ:: <code>{feels_like} °C</code>\n"
+            "ᴀɪʀ ʜᴜᴍɪᴅɪᴛʏ: <code>{air_humidity}%</code>\n"
+            "ᴡɪɴᴅ sᴘᴇᴇᴅ: <code>{wind_speed} km/h</code>\n\n"
+            "- <i>{overview}</i>"
+        ).format(
             location=loc_json["location"]["address"][0],
             temperature=obs_dict["temperature"],
             feels_like=obs_dict["temperatureFeelsLike"],
