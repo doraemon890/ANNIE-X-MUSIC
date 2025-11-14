0de7e9cc0a9c964756a10847f43a102a668a9823
Certified Coders <rajnishmishraaa1@gmail.com>
2025-11-14 22:23:33 +0530

    Revert "ok"
    This reverts commit 7966db9a96db1884be1b60c3576b747115076bf1.

--- AnnieXMedia/core/bot.py
+++ AnnieXMedia/core/bot.py
@@ -13,6 +13,7 @@ class MusicBotClient(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
+            in_memory=True,
             workers=48,
             max_concurrent_transmissions=7,
         )
