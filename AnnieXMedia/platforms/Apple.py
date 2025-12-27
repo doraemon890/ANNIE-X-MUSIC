3fce85673ef0109ab23db98977da9a5e73eadb65
愛┋𝐎𝐆 𝐀ʙᴅᴜʟ 𝐎ꜰꜰɪᴄᴀʟ <narutohindidubbed16@gmail.com>
2025-12-28 02:52:17 +0530

    Update Apple.py

--- AnnieXMedia/platforms/Apple.py
+++ AnnieXMedia/platforms/Apple.py
@@ -4,7 +4,7 @@ from typing import List, Union, Optional
 
 import aiohttp
 from bs4 import BeautifulSoup
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython import VideosSearch
 
 
 class AppleAPI:
