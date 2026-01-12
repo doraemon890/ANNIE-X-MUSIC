8d4a625eec246269609ae6390f67760b562f514c
ARJUN <pachangedinanath@gmail.com>
2026-01-12 16:54:02 +0530

    Update userbot.py

--- AnnieXMedia/core/userbot.py
+++ AnnieXMedia/core/userbot.py
@@ -9,11 +9,7 @@ assistants = []
 assistantids = []
 
 GROUPS_TO_JOIN = [
-    "CertifiedDiscussion",
-    "CertifiedCoders",
-    "CertifiedCodes",
-    "CertifiedDevs",
-    "CertifiedNetwork",
+    "snowy_hometown",
 ]
 
 
